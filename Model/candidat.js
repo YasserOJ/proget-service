3076d069acbac92eab9bd5f8bbe64dcbd5fd68fc
YasserOJ <yasser_ess@outlook.com>
2018-04-10 17:43:25 +0100

    ok

--- Model/candidat.js
+++ Model/candidat.js
@@ -13,7 +13,101 @@ var eet= mongoose.Schema({
     }
 });
 var candidatSchema = mongoose.Schema({
-
-})
+    first_name:{
+        type: String,
+        required:true
+    },
+    last_name:{
+        type: String,
+        required:true
+    },
+    birth_date:{
+        type: String,
+        required:true
+    },
+    birth_place:{
+        type: String,
+        required:true
+    },
+    nationality:{
+        type: String,
+        required:true
+    },
+    gender:{
+        type: String,
+        required:true
+    },
+    status:{
+        type: String,
+        required:true
+    },
+    adress:{
+        type: String,
+        required:true
+    },
+    country:{
+        type: String,
+        required:true
+    },
+    phone:{
+        type: String,
+        required:true
+    },
+    email:{
+        type: String,
+        required:true
+    },
+    userName:{
+        type: String,
+        required:true
+    },
+    password:{
+        type: String,
+        required:true
+    },
+    picture:{
+        type: Buffer,
+        required:true
+    },
+    level:{
+        type: String,
+        required:true
+    },
+    field:{
+        type: String,
+        required:true
+    },
+    position:{
+        type: String,
+        required:true
+    },
+    experience_years:{
+        type: String,
+        required:true
+    },
+    education:{
+        type:[eet],
+        required:true
+    },
+    experince:{
+        type: [eet],
+        required:true
+    },
+    training:{
+        type: [eet],
+        required:true
+    },
+    skills:{
+        type:[String],
+        required:true
+    },
+    language:{
+        type: [String],
+        required:true
+    },
+    file:{
+        type:[Buffer]
+    }
+});
 var Candidat = module.exports = mongoose.model('Candidat', candidatSchema);
 module.exports=Candidat;
