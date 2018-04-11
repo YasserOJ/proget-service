67a607a1446d895d757df8f7de3ee41512d3b28a
YasserOJ <yasser_ess@outlook.com>
2018-04-11 15:03:27 +0100

    ok

---
+++ Model/record.js
@@ -0,0 +1,26 @@
+var mongoose =require('mongoose');
+var recordschema = mongoose.Schema({
+    client_id:{
+        type: String,
+        required: true
+    },
+    client_name:{
+        type: String,
+        required: true
+    },
+    time:{
+        type: Date,
+        default: Date.now
+    },
+    field:{
+        type: String
+    },
+    position:{
+        type: String
+    },
+    keyword:{
+        type: String
+    }
+});
+var Record = module.exports = mongoose.model('Record', recordschema);
+module.exports=Record;
