44d7eac0f9857a7c53e1994da7f2445fb23a339b
YasserOJ <yasser_ess@outlook.com>
2018-04-11 13:42:53 +0100

    ok

---
+++ Model/option.js
@@ -0,0 +1,15 @@
+var mongoose =require('mongoose');
+var optionSchema = mongoose.Schema({
+    field:{
+        type: String,
+        required:true
+    },
+    position:{
+        type: String,
+        required:true
+    },
+    email:{
+        type: String,
+        required:true
+    },
+})
