26b9d1163f9b32bbc1730f46a322753e9d13731c
YasserOJ <yasser_ess@outlook.com>
2018-04-16 17:21:05 +0100

    ok

--- routers/recordR.js
+++ routers/recordR.js
@@ -4,30 +4,62 @@ Record = require('../Model/record');
 var bodyparser=require('body-parser');
 router.use(bodyparser.json());
 
-
+router.get('/all',function (req,res) {
+    Record.find({},function (err,result) {
+        if(err){
+            throw(err);
+        }
+        res.send(result);
+    })
+});
+router.get('/byname/:name',function (req,res) {
+    obj={client_name:req.params.name};
+    Record.find(obj,function (err,result) {
+        if(err){
+            throw (err);
+        }
+        res.send(result);
+    })
+});
+router.get('/byyear/:year',function (req,res) {
+    obj={year:req.params.year}
+    Record.find(obj,function (err,result) {
+        if(err){
+            throw (err);
+        }
+        res.send(result);
+    })
+});
+router.get('/byyear/:year/:name',function (req,res) {
+    obj={
+        year:req.params.year,
+        client_name:req.params.name
+    };
+    Record.find(obj,function (err,result) {
+        if(err){
+            throw (err);
+        }
+        res.send(result);
+    })
+});
+router.get('/byyear/:year/:month/:name',function (req,res) {
+    obj={
+        year:req.params.year,
+        month:req.params.month,
+        client_name:req.params.name
+    };
+    Record.find(obj,function (err,result) {
+        if(err){
+            throw (err);
+        }
+        res.send(result);
+    })
+});
 router.post('/register',function (req,res) {
     var ibody= req.body;
     Record.create(ibody,function (err,ibody) {
         if(err){
             throw (err);
-        }
-        res.send(ibody);
-    })
-});
-router.delete('/del/:_id',function (req,res) {
-    var obj={_id:req.params};
-    Record.findByIdAndRemove(obj,function (err,result) {
-        if(err){
-            throw err;
-        }
-        res.send(result);
-    })
-});
-router.put('/up/:_id',function (req,res) {
-    var ibody=req.body;
-    Record.remove(ibody,function (err,ibody) {
-        if(err){
-            throw err;
         }
         res.send(ibody);
     })
