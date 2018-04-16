4edbb5e31069d1e8d78b75af0160b5d024ddff13
YasserOJ <yasser_ess@outlook.com>
2018-04-16 14:58:48 +0100

    ok

--- Model/postule.js
+++ Model/postule.js
@@ -6,6 +6,26 @@ var postuleSchema = mongoose.Schema({
     },
     img:{
         type: Buffer,
+    },
+    tag:{
+      type:String,
+    },
+    year:{
+        type:Number,
+        min:2018,
+        max:3000,
+        required:true
+    },
+    month:{
+        type:Number,
+        min:1,
+        max:12,
+        required:true
+    },
+    day:{
+        type:Number,
+        min:1,
+        max:31,
         required:true
     }
 });
