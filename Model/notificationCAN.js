ce3be33cb76d8fe586bcf7feec451fd90ac47463
YasserOJ <yasser_ess@outlook.com>
2018-04-20 17:46:24 +0100

    ok

--- Model/notificationCAN.js
+++ Model/notificationCAN.js
@@ -1,5 +1,16 @@
 var mongoose =require('mongoose');
 var notificationCANSchema = mongoose.Schema({
-
+    candidat_id:{
+        type: String,
+        required:true
+    },
+    post_id:{
+        type: String,
+        required:true
+    },
+    text:{
+        type: String,
+        required:true
+    }
 });
 var NotificationCAN = module.exports = mongoose.model('NotificationCAN', notificationCANSchema);
--- Model/notificationCL.js
+++ Model/notificationCL.js
@@ -1,5 +1,16 @@
 var mongoose =require('mongoose');
 var notificationCLSchema = mongoose.Schema({
-
+    client_id:{
+        type: String,
+        required:true
+    },
+    post_id:{
+        type: String,
+        required:true
+    },
+    text:{
+        type: String,
+        required:true
+    }
 });
 var NotificationCL = module.exports = mongoose.model('NotificationCL', notificationCLSchema);
